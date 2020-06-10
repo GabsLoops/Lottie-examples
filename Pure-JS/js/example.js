a23c8c97f4b90191417cc409276c081b6a0404d9
GabsLoops <gabslopes34@gmail.com>
2020-06-09 23:59:28 -0300

    Add example using local json file

--- Pure-JS/js/example.js
+++ Pure-JS/js/example.js
@@ -1,10 +1,26 @@
-var animationConteiner = document.getElementById('animationConteiner');
-var animationOptions = {
-  wrapper: animationConteiner,
+//Use a external json data to load animation
+var animationConteiner1 = document.getElementById('animationConteiner1');
+var animationOptionsExternal = {
+  wrapper: animationConteiner1,
   animType: 'svg',
   loop: true,
   autoplay: true,
   path: 'https://raw.githubusercontent.com/GabsLoops/Lottie-examples/master/Pure-JS/animations/example-1.json'
 }
-animation = lottie.loadAnimation(animationOptions);
-animation.setSubframe(false);
+animationExternal = lottie.loadAnimation(animationOptionsExternal);
+animationExternal.setSubframe(false);
+
+
+//Use a local .json file to load animation
+var DataAnimation = JSON.parse(JSONDataAnimation);
+var animationConteiner2 = document.getElementById('animationConteiner2');
+var animationOptionsLoadedFile = {
+  wrapper: animationConteiner2,
+  animType: 'svg',
+  loop: true,
+  autoplay: true,
+  animationData: DataAnimation
+}
+animationLoadedFile = lottie.loadAnimation(animationOptionsLoadedFile);
+animationLoadedFile.setSubframe(false);
+
